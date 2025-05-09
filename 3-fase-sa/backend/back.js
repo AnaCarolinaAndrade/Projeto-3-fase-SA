dda117e1c71bc91e316f065589581b366789e7ca
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-09 20:37:23 -0300

    Conectando o back.js ao mongoDB

---
+++ 3-fase-sa/backend/back.js
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const URI = process.env.MONGO_URI;
+
+mongoose.connect(URI)
+.then(() => console.log('Conectado ao MongoDB com sucesso!'))
+.catch(err => console.error('Erro na conexão com o MongoDB:', err));
