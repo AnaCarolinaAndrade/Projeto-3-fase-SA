f4319153c2dedfce0c01e552a108e7620d495f8e
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-11 20:10:26 -0300

    Colocando mais coisas na aba Rankings

--- 3-fase-sa/src/pages/Ranking.jsx
+++ 3-fase-sa/src/pages/Ranking.jsx
@@ -9,31 +9,37 @@ const projetos = [
     { nome: "Projeto7" },
     { nome: "Projeto8" },
     { nome: "Projeto9" },
+    { nome: "Projeto11" },
+    { nome: "Projeto12" },
+    { nome: "Projeto13" },
+
 ];
 
 export default function Ranking() {
     return (
-        <div className="ranking-container">
-            <h2 className="ranking-title">Ranking projetos</h2>
-            <div className="ranking-card">
-                <h3 className="ranking-subtitle">Projetos Hypados</h3>
-                <div className="ranking-scroll">
-                    {projetos.map((projeto, index) => (
-                        <div key={index} className="ranking-item">
-                            {projeto.destaque ? (
-                                <img src="/logo.svg" alt="Logo" className="ranking-logo" />
-                            ) : (
-                                <div className="ranking-placeholder" />
-                            )}
-                            <span className="ranking-nome">{projeto.nome}</span>
-                            <div className="ranking-bar">
-                                <div
-                                    className="ranking-fill"
-                                    style={{ width: `${100 - index * 10}%` }}
-                                />
+        <div className='container-ranking'>
+            <div className="ranking-container">
+                <h2 className="ranking-title">Ranking projetos</h2>
+                <div className="ranking-card">
+                    <h3 className="ranking-subtitle">Projetos Hypados</h3>
+                    <div className="ranking-scroll">
+                        {projetos.map((projeto, index) => (
+                            <div key={index} className="ranking-item">
+                                {projeto.destaque ? (
+                                    <img src="/logo.svg" alt="Logo" className="ranking-logo" />
+                                ) : (
+                                    <div className="ranking-placeholder" />
+                                )}
+                                <span className="ranking-nome">{projeto.nome}</span>
+                                <div className="ranking-bar">
+                                    <div
+                                        className="ranking-fill"
+                                        style={{ width: `${100 - index * 10}%` }}
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
