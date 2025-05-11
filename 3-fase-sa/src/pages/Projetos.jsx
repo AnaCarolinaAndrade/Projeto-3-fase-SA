0b0f4188a1cdd91177f441612853f22013c33fda
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-10 22:17:16 -0300

    Colocando a parte do pedro

--- 3-fase-sa/src/pages/Projetos.jsx
+++ 3-fase-sa/src/pages/Projetos.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import './Projetos.css'
 
 export default function Projetos() {
     return (
         <div className='container-projetos'>
             <div className='projetos-wrapper'>
                 <div>
-                    
+                    A ABA PROJETOS QUE ERA PRO PEDRO FAZER, MAS ELE NÃO FAIZ 😡
                 </div>
             </div>
         </div>
