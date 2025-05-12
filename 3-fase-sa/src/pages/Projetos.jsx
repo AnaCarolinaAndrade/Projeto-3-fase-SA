0fd59ab7e395710371bcb71cd0befbcba06f55f4
PEDRO HENRIQUE MARQUES DE SOUZA <pedro_marques-souza@estudante.sesisenai.org.br>
2025-05-12 15:30:57 -0300

    ainda terminando

--- 3-fase-sa/src/pages/Projetos.jsx
+++ 3-fase-sa/src/pages/Projetos.jsx
@@ -5,9 +5,7 @@ export default function Projetos() {
     return (
         <div className='container-projetos'>
             <div className='projetos-wrapper'>
-                <div>
-                    A ABA PROJETOS QUE ERA PRO PEDRO FAZER, MAS ELE NÃO FAIZ 😡
-                </div>
+                
             </div>
         </div>
     )
