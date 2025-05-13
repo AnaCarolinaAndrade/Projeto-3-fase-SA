1ab12fa238c1d91ff97b282267e55ee5515f93ed
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-13 11:19:09 -0300

    Arrumando coisas da home

--- 3-fase-sa/src/pages/Home.jsx
+++ 3-fase-sa/src/pages/Home.jsx
@@ -8,19 +8,23 @@ import Footer from '../components/Footer';
 function Home() {
   return (
     <div className='Home-container'>
-      <Navbar />
-      <Sidebar />
-      <h1 className='title-home'>Descubra e Divulge projetos <br/> Web, para o mundo todo <br/> com o DEXES</h1>
-      <p className='subtitle-home'> Hub para desenvolvedores e criadores colaborarem em projetos web.
-      <br/> Conecte-se, compartilhe ideias e transforme-as em oportunidades - tudo em um só lugar.</p>
-      <img className='container-img' src="./img/landig_page.png"/>
-      <div className='degrade-container'/>
-    
-      <div className='buttons-landig'>
-       <button className='projetos-container'>Acessar Projetos</button>
-       <button className='posts-container'>Acessar Posts</button>
+      {/* <Navbar />
+      <Sidebar /> */}
+      <div className='container-home'>
+
+        <div className='container-title-home'>
+          <h1 className='title-home'>Descubra e Divulge projetos <br /> Web, para o mundo todo <br /> com o DEXES</h1>
+          <p className='subtitle-home'> Hub para desenvolvedores e criadores colaborarem em projetos web.
+            <br /> Conecte-se, compartilhe ideias e transforme-as em oportunidades - tudo em um só lugar.</p>
+          <img className='container-img' src="./img/landig_page.png" />
+          <div className='degrade-container' />
+        </div>
+
+        <div className='buttons-landig'>
+          <button className='projetos-container'>Acessar Projetos</button>
+          <button className='posts-container'>Acessar Posts</button>
+        </div>
       </div>
-      <Footer/>
     </div>
   )
 }
