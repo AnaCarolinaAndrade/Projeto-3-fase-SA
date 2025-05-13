b8e2c0099e03999305c20301c6291fbad7c953e8
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-05-13 11:44:48 -0300

    Arrumando um pouco o css da home

--- 3-fase-sa/src/pages/Home.jsx
+++ 3-fase-sa/src/pages/Home.jsx
@@ -13,17 +13,26 @@ function Home() {
       <div className='container-home'>
 
         <div className='container-title-home'>
-          <h1 className='title-home'>Descubra e Divulge projetos <br /> Web, para o mundo todo <br /> com o DEXES</h1>
-          <p className='subtitle-home'> Hub para desenvolvedores e criadores colaborarem em projetos web.
-            <br /> Conecte-se, compartilhe ideias e transforme-as em oportunidades - tudo em um só lugar.</p>
-          <img className='container-img' src="./img/landig_page.png" />
-          <div className='degrade-container' />
+          <div className='text-content'>
+            <h1 className='title-home'>Descubra e Divulgue projetos <br /> Web, para o mundo todo <br /> com o DEXES</h1>
+            <span className='subtitle-home'>
+              Hub para desenvolvedores e criadores colaborarem em projetos web.
+              <br /> Conecte-se, compartilhe ideias e transforme-as em oportunidades - tudo em um só lugar.
+            </span>
+          </div>
+
+          <div className='image-content'>
+            <img className='container-img' src="./img/landig_page.png" />
+            <div className='degrade-container' />
+          </div>
         </div>
 
         <div className='buttons-landig'>
           <button className='projetos-container'>Acessar Projetos</button>
           <button className='posts-container'>Acessar Posts</button>
-        </div>
+        </div>      
+        <Footer/>
+
       </div>
     </div>
   )
