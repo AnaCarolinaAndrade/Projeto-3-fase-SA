45869ddbe28a9801ddc661ca579946eb5a4e5346
ANA CAROLINA ANDRADE DE ARAUJO <ana_ca_araujo@estudante.sesisenai.org.br>
2025-05-28 17:11:26 -0300

    Veremos se o back que temos até agora está funcionando 100% ;)

--- 3-fase-sa/src/components/Footer.jsx
+++ 3-fase-sa/src/components/Footer.jsx
@@ -7,18 +7,18 @@ function Footer() {
       <footer className='.container-footer-2'>
         <div className='footer-section'>
         <div className='container-contatos'>
-         <h3> <img src='./img/chamada-telefonica.png' className='img-contatos'/>Contatos</h3>
-          <li> <img src='./img/whatsapp.png' className='img-whatsapp'/> 48 99455-7822</li>
-          <li> <img src='./img/email.png' className='img-email'/>DexesProject@Gmail</li>
-          <li> <img src='./img/instagram.png' className='img-instagram'/>dexes_project</li>
+         <h3>Contatos</h3>
+          <li>48 99455-7822</li>
+          <li>DexesProject@Gmail</li>
+          <li>dexes_project</li>
         </div>
         <div className='line-2'></div>
 
         <div className='container-menu'>
-         <h3> <img src='./img/menu.png' className='img-menu'/>Menu do site</h3>
-         <li> <img src='./img/chat.png' className='img-chat'/>Chat</li>
-         <li> <img src='./img/Posts.png' className='img-posts'/>Postagens</li>
-         <li> Perfil</li>
+         <h3>Menu do site</h3>
+         <li>Chat</li>
+         <li>Postagens</li>
+         <li>Perfil</li>
          <li>Ranking</li>
          <li>Projetos</li>
          <li>Criar Projetos</li>
