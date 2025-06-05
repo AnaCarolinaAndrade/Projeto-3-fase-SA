c01c95f940f5c96b717f37d06c9f5c53012324f0
L7noxy <gabrielcastanhel2@gmail.com>
2025-06-05 16:47:56 -0300

    ./

--- 3-fase-sa/src/pages/CriarProjeto.jsx
+++ 3-fase-sa/src/pages/CriarProjeto.jsx
@@ -1,7 +1,12 @@
+import './CriarProjeto.css'
+
 export default function CriarProjeto() {
   return (
-    <div>
-      
+    <div className='container-criacao'>
+      <div className='criar-projeto'>
+        <h3>Nome do projeto: </h3>
+        <input type="text" />
+      </div>
     </div>
   )
 }
