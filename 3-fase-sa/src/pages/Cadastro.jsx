9c9c509735aff04b653d0da875196e8a4b9703c8
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-06-18 12:59:37 -0300

    .

--- 3-fase-sa/src/pages/Cadastro.jsx
+++ 3-fase-sa/src/pages/Cadastro.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, } from 'react';
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
-import axios from 'axios';
 import "./Cadastro.css";
 
 export default function Cadastro() {
